1faa166f8819541830b8716bb455eda84d073bc8
Kareen Salame <itskareen3@hotmail.com>
2024-04-02 14:44:12 +0300

    Your commit message

---
+++ app/src/main/java/com/example/androidapp2024/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.androidapp2024
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+}
