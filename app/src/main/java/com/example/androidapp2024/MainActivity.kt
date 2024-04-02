4b186a417986f9c0797d7876fde7d2d161da2b52
Kareen Salame <itskareen3@hotmail.com>
2024-04-03 01:00:34 +0300

    test

--- app/src/main/java/com/example/androidapp2024/MainActivity.kt
+++ app/src/main/java/com/example/androidapp2024/MainActivity.kt
@@ -1,33 +1,49 @@
 package com.example.androidapp2024
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("DEMO", "OnCreate")
+        val intent = Intent(this, AddUserActivity::class.java)
+        startActivity(intent)
     }
 
     override fun onStart() {
         super.onStart()
+        Log.d("DEMO", "onStart")
+
     }
 
     override fun onResume() {
         super.onResume()
+        Log.d("DEMO", "onResume")
+
     }
 
     override fun onPause() {
         super.onPause()
+        Log.d("DEMO", "onPause")
+
     }
 
     override fun onStop() {
         super.onStop()
+        Log.d("DEMO", "onStop")
+
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        Log.d("DEMO", "onDestroy")
+
     }
 
 }
