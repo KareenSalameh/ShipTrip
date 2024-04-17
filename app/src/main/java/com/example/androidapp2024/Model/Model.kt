d128386477d71cdda46c2892d2288a8ee0af29bd
Kareen Salame <itskareen3@hotmail.com>
2024-04-18 02:07:31 +0300

    UI login+register

---
+++ app/src/main/java/com/example/androidapp2024/Model/Model.kt
@@ -0,0 +1,18 @@
+package com.example.androidapp2024.Model
+
+import android.media.audiofx.AudioEffect.Descriptor
+
+class Model private constructor() {
+    val users: MutableList<User> = ArrayList()
+
+    companion object {
+        val instance: Model = Model()
+    }
+
+    init {
+        for (i in 0..20){
+            val user = User("Name: $i", "ID: $i", "https://me.com/avatar.jpg", false, "Description: $i")
+        users.add(user)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/androidapp2024/Model/User.kt
@@ -0,0 +1,8 @@
+package com.example.androidapp2024.Model
+
+data class User(val name: String,
+                val id: String,
+                val avatar: String,
+                var isChecked: Boolean,
+                val description: String,) {
+}
